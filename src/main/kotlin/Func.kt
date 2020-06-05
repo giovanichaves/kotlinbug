e193bef33baa7e2c66f879899537b1840511bae1
Giovani Chaves <giovani.chaves@spark.net>
2020-06-05 14:22:10 +0200

    init

---
+++ src/main/kotlin/Func.kt
@@ -0,0 +1,39 @@
+package main.kotlin
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+object Func {
+
+//    //WORKS
+//    @JvmStatic
+//    fun round(number: Double, places: Int): Double? {
+//        if (number == null || places == null) return null
+//
+//        return round(number.toBigDecimal(), places)?.toDouble()
+//    }
+//
+//    @JvmStatic
+//    fun round(number: BigDecimal, places: Int): BigDecimal? {
+//        if (number == null || places == null) return null
+//
+//        return number.setScale(places, RoundingMode.HALF_UP)
+//    }
+
+
+
+    // FAILS
+    @JvmStatic
+    fun round(number: Double?, places: Int?): Double? {
+        if (number == null || places == null) return null
+
+        return round(number.toBigDecimal(), places)?.toDouble()
+    }
+
+    @JvmStatic
+    fun round(number: BigDecimal?, places: Int?): BigDecimal? {
+        if (number == null || places == null) return null
+
+        return number.setScale(places, RoundingMode.HALF_UP)
+    }
+}
---
+++ src/main/kotlin/Use.java
@@ -0,0 +1,11 @@
+package main.kotlin;
+
+public class Use {
+
+    private Short a;
+    private Short b;
+
+    public void example() {
+        Func.round(a*2/b, 3);
+    }
+}
